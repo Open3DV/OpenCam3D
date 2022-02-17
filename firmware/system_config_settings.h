576e60ff02d61b93ca2c174e0a06bebad3bea9b8
zhangguanjin <zhangguanjin@dexforce.com>
2022-02-17 16:56:08 +0800

    添加外参等系统配置参数

--- firmware/system_config_settings.h
+++ firmware/system_config_settings.h
@@ -17,6 +17,17 @@ struct SystemConfigParam
 	int exposure_num;
 	//曝光参数
 	int exposure_param[6];
+	//相机曝光时间（us）
+	float camera_exposure_time;
+	//相机增益DB
+	float camera_gain;
+	//外参标识:1-相机、2-光机、3-外部标定板。
+	int external_param_flag;
+	//相机外参
+	float external_param[12];
+	//基准平面
+	float standard_plane[4];
+
 };
 
 struct SystemConfigDataStruct
