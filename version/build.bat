5e1b28224b1639a061f62cfa559bd17ff7d7f057
wangtong <tong_wang@163.com>
2022-03-04 16:04:31 +0800

    完善版本号功能，使解决方案编译时自动执行脚本，自动生成头文件version.h，自动编译进程序—— 1、安装cygwin，须在windows下使用shell； 2、sdk project增加生成前事件，生成时执行build.bat 3、build.bat添加环境变量，再调用.sh文件 4、build.sh文件自动生成version.h文件 5、version.h被编译进程序

---
+++ version/build.bat
@@ -0,0 +1,4 @@
+@echo off
+set PATH=%PATH%;C:/cygwin64/bin
+cd
+bash "./build.sh"
--- version/build.sh
+++ version/build.sh
@@ -5,5 +5,3 @@ current_time=`date +"%Y-%m-%d %H:%M:%S"`
 git_version=`git log -1 --format="%h"`
 camera_model='DFX802'
 sed s/MYVERSION/"OpenCam3D-$camera_model version: $git_version commit: $commit_time build: $current_time"/g version.h.tmp > ../firmware/version.h
-#make clean
-#make
