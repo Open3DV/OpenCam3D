15570d21274283cd69d96c16fd87e19510b1af56
wangtong <wangtong@dexforce.com>
2022-06-24 10:06:12 +0800

    解决VC工程编译版本号存在的问题

--- version/build.sh
+++ version/build.sh
@@ -4,4 +4,4 @@ commit_time=`date -d@$commit_ts +"%Y-%m-%d %H:%M:%S"`
 current_time=`date +"%Y-%m-%d %H:%M:%S"`
 git_version=`git log -1 --format="%h"`
 camera_model='DFX802'
-sed s/MYVERSION/"OpenCam3D-$camera_model version: $git_version commit: $commit_time build: $current_time"/g version.h.tmp > ../firmware/version.h
+sed s/MYVERSION/"OpenCam3D-$camera_model\ version: $git_version\ commit: $commit_time\ build: $current_time"/g version.h.tmp > ../firmware/version.h
