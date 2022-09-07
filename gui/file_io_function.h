1a649fe54c6b5950fd5ee12131a73edd8790e41a
zhangguanjin <zhangguanjin@dexforce.com>
2022-09-07 01:58:09 -0700

    修复linux下编译出错

--- gui/file_io_function.h
+++ gui/file_io_function.h
@@ -53,7 +53,7 @@ public:
 	//xyz-map图转z-map图
 	bool mapToColor(cv::Mat deep_map, cv::Mat &color_map, cv::Mat &grey_map,int low_z, int high_z);
 
-	int FileIoFunction::percentile(cv::Mat& image, int percent);
+	int percentile(cv::Mat& image, int percent);
 
 	//深度图转z-map图
 	bool depthToColor(cv::Mat depth_map, cv::Mat& color_map, cv::Mat& grey_map, int low_z, int high_z);
